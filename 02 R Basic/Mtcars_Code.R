d31b197de8bfa3e1884a0443fd737d6fdf1d5e34
Eunbi Kim <aqua9131@gmail.com>
2018-01-16 12:03:51 +0900

    Mtcars exercise code, data set 추가

---
+++ 02 R Basic/Mtcars_Code.R
@@ -0,0 +1,12 @@
+cars<-read.table("02 R Basic/mtcars.txt", header=T)  #txt 파일 읽어드리기
+
+summary(cars) #속성 별 값 분포
+
+head(cars, n=10)  #default : 데이터의 첫 6줄 출력
+tail(cars, n=10)  #default : 데이터의 마지막 6줄 출력
+
+rownames(cars)  #데이터의 행 이름 출력
+colnames(cars)  #데이터의 열 이름 출력
+
+cars$mpg  #데이터의 속성 선택
+
